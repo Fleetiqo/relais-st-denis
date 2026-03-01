8cc14651977e14e7379715447ec7428693a77ae5
Fleetiqo <contact@fleetiqo.com>
2026-02-28 22:13:22 -0500

    Fix deployment workflow to match working config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/relais-st-denis/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
-  }
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
